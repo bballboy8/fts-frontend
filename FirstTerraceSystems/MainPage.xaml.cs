32313904bef304076186b19dc2d757e48814634c
Pradip Gajera <pradip.gajera1984@live.com>
2024-05-02 14:01:16 +0530

    Initial project setup

---
+++ FirstTerraceSystems/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstTerraceSystems
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FirstTerraceSystems/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FirstTerraceSystems
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
