332269bf328c5394786b0ec9be3bc245bb7fb3ec
Pradip <pradip@teamopine.com>
2024-05-11 13:34:45 +0530

    Added Theam functionality

--- FirstTerraceSystems/wwwroot/js/stockchart.js
+++ FirstTerraceSystems/wwwroot/js/stockchart.js
@@ -1,265 +1,5 @@
-﻿//window.loadStockChart = async () => {
-//    const data = await fetch(
-//        'https://demo-live-data.highcharts.com/aapl-ohlcv.json'
-//    ).then(response => response.json());
-
-//    const ohlc = [],
-//        volume = [],
-//        dataLength = data.length,
-//        groupingUnits = [['week', [1]], ['month', [1, 2, 3, 4, 6]]];
-
-//    for (let i = 0; i < dataLength; i += 1) {
-//        ohlc.push([data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]]);
-//        volume.push([data[i][0], data[i][5]]);
-//    }
-
-//    Highcharts.stockChart('container', {
-//        chart: {
-//            backgroundColor: "#202527",
-//            borderWidth: 1,
-//            borderColor: "#5B6970",
-//            //marginRight: 100, //margin after chart
-//        },
-//        rangeSelector: {
-//            selected: 4
-//        },
-//        xAxis: [{
-//            offset: 0,
-//            labels: {
-//                align: 'left',
-//                x: 5,
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            lineWidth: 0,
-//            opposite: false
-//        }, {
-//            offset: 0,
-//            labels: {
-//                align: 'left',
-//                x: 5,
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            lineWidth: 0,
-//            opposite: false
-//        }],
-//        /*title: {
-//            text: 'AAPL Historical'
-//        },*/
-//        yAxis: [{
-//            labels: {
-//                align: 'left',
-//                x: 5,
-//                style: {
-//                    color: '#ffffff' // Green color
-//                }
-//            },
-//            /* title: {
-//                 text: 'OHLC',
-//                 style: {
-//                     color: '#ffffff',
-//                     fontSize: 18
-//                 },
-//             },*/
-//            height: '65%',
-//            lineWidth: 2,
-//            resize: {
-//                enabled: true
-//            }
-//        }, {
-//            labels: {
-//                align: 'left',
-//                x: 5,
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            //title: {
-//            //    text: 'VOLUME',
-//            //    style: {
-//            //        color: '#5B6970',
-//            //        fontSize: 13,
-//            //        fontweight:"600"
-//            //    },
-//            //},
-//            top: '65%',
-//            height: '35%',
-//            offset: 0,
-//            gridLineWidth: 0,
-//            lineWidth: 2
-//        }],
-//        tooltip: {
-//            split: true
-//        },
-//        series: [{
-//            type: 'candlestick',
-//            name: 'AAPL',
-//            data: ohlc,
-//            dataGrouping: {
-//                units: groupingUnits
-//            },
-//            color: '#C01620', // Color for the fall
-//            upColor: '#16C05A', // Color for the rise
-//        }, {
-//            type: 'column',
-//            name: 'Volume',
-//            data: volume,
-//            yAxis: 1,
-//            dataGrouping: {
-//                units: groupingUnits
-//            }
-//        }],
-//        exporting: {
-//            enabled: false // Disable export
-//        }
-//    });
-//};
-
-//window.loadStockChart1 = async () => {
-//    const data = await fetch(
-//        'https://demo-live-data.highcharts.com/aapl-ohlcv.json'
-//    ).then(response => response.json());
-
-//    // split the data set into ohlc and volume
-//    const ohlc = [],
-//        volume = [],
-//        dataLength = data.length;
-
-//    for (let i = 0; i < dataLength; i += 1) {
-//        ohlc.push([
-//            data[i][0], // the date
-//            data[i][1], // open
-//            data[i][2], // high
-//            data[i][3], // low
-//            data[i][4] // close
-//        ]);
-
-//        volume.push([
-//            data[i][0], // the date
-//            data[i][5] // the volume
-//        ]);
-//    }
-
-//    Highcharts.stockChart('container1', {
-//        chart: {
-//            backgroundColor: "#202527",
-//            borderWidth: 1,
-//            borderColor: "#5B6970",
-//        },
-//        xAxis: [{
-//            offset: 0,
-//            labels: {
-//                align: 'left',
-//                x: 5,
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            lineWidth: 0,
-//            opposite: false
-//        }, {
-//            offset: 0,
-//            labels: {
-//                align: 'left',
-//                x: 5,
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            lineWidth: 0,
-//            opposite: false
-//        }],
-//        yAxis: [{
-//            labels: {
-//                align: 'left',
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            height: '80%',
-//            resize: {
-//                enabled: true
-//            }
-//        }, {
-//            labels: {
-//                align: 'left',
-//                style: {
-//                    color: '#ffffff'
-//                }
-//            },
-//            top: '80%',
-//            height: '20%',
-//            offset: 0
-//        }],
-//        tooltip: {
-//            shape: 'square',
-//            headerShape: 'callout',
-//            borderWidth: 0,
-//            shadow: false,
-//            positioner: function (width, height, point) {
-//                const chart = this.chart;
-//                let position;
-
-//                if (point.isHeader) {
-//                    position = {
-//                        x: Math.max(
-//                            // Left side limit
-//                            chart.plotLeft,
-//                            Math.min(
-//                                point.plotX + chart.plotLeft - width / 2,
-//                                // Right side limit
-//                                chart.chartWidth - width - chart.marginRight
-//                            )
-//                        ),
-//                        y: point.plotY
-//                    };
-//                } else {
-//                    position = {
-//                        x: point.series.chart.plotLeft,
-//                        y: point.series.yAxis.top - chart.plotTop
-//                    };
-//                }
-
-//                return position;
-//            }
-//        },
-//        series: [{
-//            type: 'ohlc',
-//            id: 'aapl-ohlc',
-//            name: 'AAPL Stock Price',
-//            data: ohlc,
-//            color: '#C01620', // Color for the fall
-//            upColor: '#16C05A',
-//        }, {
-//            type: 'column',
-//            id: 'aapl-volume',
-//            name: 'AAPL Volume',
-//            data: volume,
-//            yAxis: 1,
-//            gridLineWidth: 0
-//        }],
-//        responsive: {
-//            rules: [{
-//                condition: {
-//                    maxWidth: 800
-//                },
-//                chartOptions: {
-//                    rangeSelector: {
-//                        inputEnabled: false
-//                    }
-//                }
-//            }]
-//        },
-//        exporting: {
-//            enabled: false // Disable export
-//        }
-//    });
-//}
-
-
+﻿let backgroundColor = '#202527';
+let fontColor = '#ffffff';
 
 window.loadStockChart = async () => {
     const data = await fetch(
@@ -278,7 +18,7 @@ window.loadStockChart = async () => {
 
     Highcharts.stockChart('container', {
         chart: {
-            backgroundColor: "#202527",
+            backgroundColor: backgroundColor,
             borderWidth: 1,
             borderColor: "#5B6970",
             //marginRight: 100, //margin after chart
@@ -292,7 +32,7 @@ window.loadStockChart = async () => {
                 align: 'left',
                 x: 5,
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             lineWidth: 0,
@@ -303,7 +43,7 @@ window.loadStockChart = async () => {
                 align: 'left',
                 x: 5,
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             lineWidth: 0,
@@ -317,7 +57,7 @@ window.loadStockChart = async () => {
                 align: 'left',
                 x: 5,
                 style: {
-                    color: '#ffffff' // Green color
+                    color: fontColor // Green color
                 }
             },
             /* title: {
@@ -337,7 +77,7 @@ window.loadStockChart = async () => {
                 align: 'left',
                 x: 5,
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             //title: {
@@ -408,7 +148,7 @@ window.loadStockChart1 = async () => {
 
     Highcharts.stockChart('container1', {
         chart: {
-            backgroundColor: "#202527",
+            backgroundColor: backgroundColor,
             borderWidth: 1,
             borderColor: "#5B6970",
         },
@@ -418,7 +158,7 @@ window.loadStockChart1 = async () => {
                 align: 'left',
                 x: 5,
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             lineWidth: 0,
@@ -429,7 +169,7 @@ window.loadStockChart1 = async () => {
                 align: 'left',
                 x: 5,
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             lineWidth: 0,
@@ -439,7 +179,7 @@ window.loadStockChart1 = async () => {
             labels: {
                 align: 'left',
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             height: '80%',
@@ -450,7 +190,7 @@ window.loadStockChart1 = async () => {
             labels: {
                 align: 'left',
                 style: {
-                    color: '#ffffff'
+                    color: fontColor
                 }
             },
             top: '80%',
@@ -521,3 +261,57 @@ window.loadStockChart1 = async () => {
         }
     });
 }
+
+window.changeBackgroundColor = (isDarkMode) => {
+    // Change HTML body background color based on the isDarkMode parameter
+    fontColor = isDarkMode ? '#ffffff' : '#202527';
+    backgroundColor = isDarkMode ? '#202527' : '#ffffff';
+    document.body.style.backgroundColor = isDarkMode ? '#202527' : '#ffffff';
+    document.getElementById("headbar").style.backgroundColor = isDarkMode ? '#202527' : '#ffffff';
+
+    // Update text color for all axes labels
+    document.querySelectorAll('.highcharts-xaxis-labels text, .highcharts-yaxis-labels text').forEach(label => {
+        label.style.fill = fontColor;
+    });
+
+    // Update chart backgrounds and series colors for all Highcharts charts
+    Highcharts.charts.forEach(chart => {
+        chart.update({
+            chart: {
+                backgroundColor: backgroundColor,
+                borderColor: isDarkMode ? "#5B6970" : "#ffffff",
+            },
+            xAxis: [{
+                labels: {
+                    style: {
+                        color: fontColor
+                    }
+                }
+            }, {
+                labels: {
+                    style: {
+                        color: fontColor
+                    }
+                }
+            }],
+            yAxis: [{
+                labels: {
+                    style: {
+                        color: fontColor
+                    }
+                }
+            }, {
+                labels: {
+                    style: {
+                        color: fontColor
+                    }
+                }
+            }],
+            series: chart.series.map(series => ({
+                ...series.options,
+                color: isDarkMode ? '#C01620' : '#16C05A', // Fall or rise color
+                upColor: isDarkMode ? '#16C05A' : '#C01620' // Rise or fall color
+            }))
+        });
+    });
+}; 
