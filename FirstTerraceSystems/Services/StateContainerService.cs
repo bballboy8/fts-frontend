401d9880fc9e8d7db37866c0d69331531fb636aa
to-sandip-rathva <sandipr@teamopine.com>
2024-06-11 17:56:07 +0530

    popin and popout charts

---
+++ FirstTerraceSystems/Services/StateContainerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorBootstrap;
+using FirstTerraceSystems.Models;
+using Microsoft.JSInterop;
+
+namespace FirstTerraceSystems.Services
+{
+    internal class StateContainerService
+    {
+        public List<IJSRuntime>? JSRuntimes { get; set; }
+        public List<ChartPageModal>? ChartPages { get; set; }
+
+        public void AddChartPage(ChartPageModal chartPage)
+        {
+            ChartPages ??= new();
+            ChartPages.Add(chartPage);
+        }
+
+        public void RemoveChartPage(string chartId)
+        {
+            var page = ChartPages?.FirstOrDefault(cp => cp.ChartId == chartId);
+            if (page != null)
+            {
+                ChartPages?.Remove(page);
+            }
+        }
+
+        public void AddJSRuntimes(IJSRuntime jSRuntime)
+        {
+            JSRuntimes ??= new();
+            JSRuntimes.Add(jSRuntime);
+        }
+    }
+}
--- FirstTerraceSystems/wwwroot/js/MultiChart.js
+++ FirstTerraceSystems/wwwroot/js/MultiChart.js
@@ -1,8 +1,8 @@
 ﻿
 var ohlc = [], volume = [], dataLength = 0, groupingUnits = [['week', [1]], ['month', [1, 2, 3, 4, 6]]];
 
-
-function addChart(charContainerId) {
+function addChart(charContainerId, ohlc, volume, groupingUnits, isDragable = true) {
+    
     Highcharts.stockChart(charContainerId, {
         chart: {
             backgroundColor: backgroundColor,
@@ -145,50 +145,50 @@ function addChart(charContainerId) {
         tooltip: {
             split: true
         },
-        series: [{
-            name: 'AAPL',
-            data: ohlc.map(function (point, i) {
-                if (i === 0 || point[1] > ohlc[i - 1][1]) {
-                    return { x: point[0], y: point[1], color: 'green' }; // Higher or first point
-                } else {
-                    return { x: point[0], y: point[1], color: 'red' }; // Lower
+        series: [
+            {
+                name: 'AAPL',
+                data: ohlc.map(function (point, i) {
+                    if (i === 0 || point[1] > ohlc[i - 1][1]) {
+                        return { x: point[0], y: point[1], color: 'green' }; // Higher or first point
+                    } else {
+                        return { x: point[0], y: point[1], color: 'red' }; // Lower
+                    }
+                }),
+                color: '#C01620', // Color for the fall
+                upColor: '#16C05A', // Color for the rise
+                lineWidth: 0,
+                marker: {
+                    enabled: true,
+                    radius: 4
+                },
+                tooltip: {
+                    valueDecimals: 2
+                },
+                states: {
+                    hover: {
+                        lineWidthPlus: 0
+                    }
                 }
-            }),
-            color: '#C01620', // Color for the fall
-            upColor: '#16C05A', // Color for the rise
-            lineWidth: 0,
-            marker: {
-                enabled: true,
-                radius: 4
             },
-            tooltip: {
-                valueDecimals: 2
-            },
-            states: {
-                hover: {
-                    lineWidthPlus: 0
-                }
+            {
+                type: 'column',
+                name: 'Volume',
+                data: volume,
+                yAxis: 1,
+                dataGrouping: {
+                    units: groupingUnits
+                },
+                color: isDarkMode ? '#C01620' : '#16C05A', // Fall or rise color
+                upColor: isDarkMode ? '#16C05A' : '#C01620' // Rise or fall color
             }
-        },
-        {
-            type: 'column',
-            name: 'Volume',
-            data: volume,
-            yAxis: 1,
-            dataGrouping: {
-                units: groupingUnits
-            },
-            color: isDarkMode ? '#C01620' : '#16C05A', // Fall or rise color
-            upColor: isDarkMode ? '#16C05A' : '#C01620' // Rise or fall color
-        }],
+        ],
         exporting: {
             buttons: {
                 contextButton: {
                     enabled: false,
                 },
                 closeButton: {
-                    x: 0,
-                    y: 0,
                     enabled: true,
                     className: 'btn btn-sm',
                     theme: {
@@ -210,8 +210,43 @@ function addChart(charContainerId) {
                         }
                     },
                     text: 'XNYS:SPX &nbsp &nbsp ✖',
-                    onclick: function (e) {
+                    //onclick: function (e) {
+                    //    removeChart(this);
+                    //},
+                    ...(isDragable ? {
+                        onclick: function (e) {
+                            removeChart(this);
+                        }
+                    } : {})
+                },
+                dragButton: {
+                    x: 360,
+                    y: 0,
+                    enabled: isDragable,
+                    className: 'btn btn-sm btn-drag ',
+                    theme: {
+                        fill: '#5B6970', // Set the button background color
+                        stroke: '#5B6970', // Set the button border color
+                        //'stroke-width': 2, // Set the button border width
+                        style: {
+                            color: '#FFFFFF', // Set the button text color
+                            //fontWeight: 'bold' // Set the button text font weight
+                        },
+                        states: {
+                            hover: {
+                                fill: '#5B6970', // Set the button background color on hover
+                                stroke: '#5B6970', // Set the button border color on hover
+                            },
+                        }
+                    },
+                    text: '✥',
+                    onclick: async function (e) {
+                        var jsObjectReference = DotNet.createJSObjectReference(window);
+                        console.log(this);
+
+                        var chartId = $(this.renderTo).data("chart-id");
                         removeChart(this);
+                        await DotNet.invokeMethodAsync('FirstTerraceSystems', 'DragedChartWindow', jsObjectReference, chartId, ohlc, volume, groupingUnits)
                     },
                 }
             },
@@ -231,7 +266,7 @@ function addChart(charContainerId) {
 
 function removeChart(chart) {
 
-    if ($("#chartList .chart-box").length == 1) 
+    if ($("#chartList .chart-box").length == 1)
         return;
 
     var chartContaner = $(chart.renderTo);
@@ -273,7 +308,6 @@ function removeChart(chart) {
 
 }
 
-
 function addChartBox(totalCharts, chartIndx) {
     var cssClass = "col-12";
     if (totalCharts == 2 || totalCharts == 4) {
@@ -298,7 +332,7 @@ function addChartBox(totalCharts, chartIndx) {
         $("#chartList .chart-box").addClass('chart-height-100');
     }
 
-    addChart(chartContainerId);
+    addChart(chartContainerId, ohlc, volume, groupingUnits);
 
     if (totalCharts > 1) {
         //addChartDblClickListener(chartContainerId);
@@ -329,7 +363,7 @@ function addChartBox(totalCharts, chartIndx) {
                     $(".chart-container", chartBox).off("dblclick");
                     return;
                 }
-                    
+
 
                 $(this).addClass("chart-popup");
                 var chartIndx = $(this).data("chart-id");
@@ -341,7 +375,77 @@ function addChartBox(totalCharts, chartIndx) {
             }
         });
     }
+}
 
+function popoutChartWindow(element, chartIndx, ohlc, volume, groupingUnits) {
+    var chartContainerId = "chart-" + chartIndx, chartBoxClass = "chart-box-" + chartIndx;
+    var chartBox = $(`<div class="chart-box ${chartBoxClass} vh-100"><div class="chart-container" id="${chartContainerId}" data-chart-id="${chartIndx}" ></div></div>`);
+    $(element).append(chartBox);
+    addChart(chartContainerId, ohlc, volume, groupingUnits, false);
+}
+
+function popinChartWindow(chartIndx, ohlc, volume, groupingUnits) {
+
+    var totalCharts = $("#chartList .chart-box").length + 1;
+
+    //Reset Chart Box
+    var cssClass = "col-12";
+    if (totalCharts == 1) {
+        cssClass = "col-12";
+    }
+    else if (totalCharts <= 4) {
+        cssClass = "col-6";
+    }
+    else if (totalCharts <= 6) {
+        cssClass = "col-4";
+    }
+    else if (totalCharts <= 8) {
+        cssClass = "col-3";
+    }
+
+    $("#chartList .chart-box").removeClass('col-3').removeClass('col-4').removeClass('col-6').removeClass('col-12');
+    $("#chartList .chart-box").addClass(cssClass);
+
+
+    var chartContainerId = "chart-" + chartIndx, chartBoxClass = "chart-box-" + chartIndx;
+    var chartBox = $(`<div class="chart-box ${chartBoxClass} ${cssClass}"><div class="chart-container" id="${chartContainerId}" data-chart-id="${chartIndx}" ></div></div>`);
+
+    $("#chartList").append(chartBox);
+
+    //Add Chart Height Class
+    if (totalCharts > 2) {
+        $("#chartList .chart-box").removeClass('chart-height-100');
+        $("#chartList .chart-box").addClass('chart-height-50');
+    }
+    else {
+        $("#chartList .chart-box").removeClass('chart-height-50');
+        $("#chartList .chart-box").addClass('chart-height-100');
+    }
+
+    addChart(chartContainerId, ohlc, volume, groupingUnits);
+
+    $(".chart-container", chartBox).on("dblclick", function () {
+        if ($(this).hasClass("chart-popup")) {
+            $(this).removeClass("chart-popup");
+            var chartIndx = $(this).data("chart-id");
+            $(this).appendTo($(".chart-box-" + chartIndx));
+            $("#popup-chart-" + chartIndx).remove();
+        }
+        else {
+            if ($("#chartList .chart-box").length == 1) {
+                $(".chart-container", chartBox).off("dblclick");
+                return;
+            }
+
+            $(this).addClass("chart-popup");
+            var chartIndx = $(this).data("chart-id");
+            var opUpChart = $(`<div id="popup-chart-${chartIndx}" style="height:50vh;width:50vw;position:absolute;top:10Vh;left:10vw;"></div>`);
+            $(this).appendTo(opUpChart);
+            opUpChart.resizable();
+            opUpChart.draggable();
+            $("body").append(opUpChart);
+        }
+    });
 }
 
 function createDashboard(totalCharts) {
@@ -359,6 +463,7 @@ function createDashboard(totalCharts) {
         $("#chartList").sortable({ disabled: false });
     }
 }
+
 async function LoadData() {
     if (dataLength == 0) {
         const data = await fetch(
