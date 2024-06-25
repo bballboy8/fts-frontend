5e271f9fe03026bddcdd13247a0a048b8f8fbfe3
greendreamer <chrispalman317@gmail.com>
2024-06-25 19:05:07 -0400

    Changed backend api url

--- FirstTerraceSystems/Services/NsdaqService.cs
+++ FirstTerraceSystems/Services/NsdaqService.cs
@@ -1,96 +1,39 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using FirstTerraceSystems.Entities.Nasdaq;
+
 namespace FirstTerraceSystems.Services
 {
     public class NsdaqService
     {
-        private string? _token;
-        private DateTime _tokenExpirationTime;
         private readonly HttpClient _client;
 
         public NsdaqService()
         {
-            _client = new HttpClient(new HttpClientHandler
+            _client = new HttpClient();
+        }
+
+        public async Task<IEnumerable<EquitiesBarModal>?> GetEquitiesBars(DateTime startDate, string symbol)
+        {
+            var requestUrl = "http://52.0.33.126:8000/nasdaq/get_data";
+            var requestData = new
             {
-                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-            });
-            _client.BaseAddress = new Uri("https://restapi.clouddataservice.nasdaq.com");
-        }
+                start_datetime = startDate.ToString("yyyy-MM-ddTHH:mm"),
+                symbol = symbol
+            };
 
-
-        private async Task InitializeTokensAsync()
-        {
-            _token = await SecureStorage.GetAsync("NasdaqAuthToken");
-            var expirationTime = await SecureStorage.GetAsync("NasdaqTokenExpirationTime");
-            _tokenExpirationTime = DateTime.TryParse(expirationTime, out var expiration) ? expiration : DateTime.MinValue;
-        }
-
-        private async Task EnsureTokenIsValidAsync()
-        {
-            await InitializeTokensAsync();
-            if (DateTime.UtcNow >= _tokenExpirationTime)
-            {
-                await RefreshTokenAsync();
-            }
-        }
-
-        private async Task SaveTokensAsync(string token, DateTime tokenExpirationTime)
-        {
-            await SecureStorage.SetAsync("NasdaqAuthToken", token);
-            await SecureStorage.SetAsync("NasdaqTokenExpirationTime", tokenExpirationTime.ToString("o"));
-        }
-
-        private async Task RefreshTokenAsync()
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, "/v1/auth/token");
-
-            string jsonData = JsonSerializer.Serialize(new
-            {
-                client_id = "firstterracesystem-kristopher-gontzes",
-                client_secret = "xNptETKzwUAyrQlSrD9WmIZ8kJyZgQSO"
-            });
-
-            request.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var requestContent = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
 
             try
             {
-                var response = await _client.SendAsync(request);
-                response.EnsureSuccessStatusCode();
-                var contentString = await response.Content.ReadAsStringAsync();
-                var token = JsonSerializer.Deserialize<TokenResponse>(contentString);
-
-                _token = token!.AccessToken;
-                _tokenExpirationTime = DateTime.UtcNow.AddSeconds(token!.ExpiresIn);
-
-                await SaveTokensAsync(_token!, _tokenExpirationTime);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        public async Task<IEnumerable<EquitiesBarModal>?> GetEquitiesBars(DateTime startDate, DateTime endDate, string symbol)
-        {
-            await EnsureTokenIsValidAsync();
-
-            string startDateString = startDate.ToString("yyyy-MM-ddTHH:mm");
-            string endDateString = endDate.ToString("yyyy-MM-ddTHH:mm");
-            string relativePath = $"/v1/nasdaq/delayed/equities/bars/{symbol}/1minute/{startDateString}/{endDateString}";
-
-            var request = new HttpRequestMessage(HttpMethod.Get, relativePath);
-
-            request.Headers.Add("Authorization", $"Bearer {_token}");
-
-            try
-            {
-                var response = await _client.SendAsync(request);
+                var response = await _client.PostAsync(requestUrl, requestContent);
                 response.EnsureSuccessStatusCode();
                 var contentString = await response.Content.ReadAsStringAsync();
                 var equitiesBars = JsonSerializer.Deserialize<IEnumerable<EquitiesBarModal>>(contentString);
-
                 return equitiesBars;
             }
             catch (Exception ex)
