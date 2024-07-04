29600c446d861b4e63ff2fad59fbeb22998ddd23
to-sandip-rathva <sandipr@teamopine.com>
2024-07-04 21:36:55 +0530

    07042024_MovedProgressiveTaskLoadingScreen

--- FirstTerraceSystems/ChartWindowPage.xaml.cs
+++ FirstTerraceSystems/ChartWindowPage.xaml.cs
@@ -43,34 +43,6 @@ public partial class ChartWindowPage : ContentPage
             { "MaxPoint", _max },
             { "Symbol", _symbol },
         };
-
-        //#if WINDOWS
-
-        //if (BlazorWebView?.Handler?.PlatformView is Microsoft.UI.Xaml.Controls.WebView2 webView2)
-        //{
-        //    await webView2.EnsureCoreWebView2Async();
-
-        //    webView2.IsTabStop = true;
-        //    webView2.IsDoubleTapEnabled = false;
-        //    webView2.HighContrastAdjustment = Microsoft.UI.Xaml.ElementHighContrastAdjustment.None;
-
-        //    CoreWebView2Settings settings = webView2.CoreWebView2.Settings;
-
-        //    settings.IsZoomControlEnabled = false;
-        //    settings.AreBrowserAcceleratorKeysEnabled = false;
-        //    settings.AreDefaultContextMenusEnabled = false;
-        //    settings.AreDefaultScriptDialogsEnabled = false;
-        //    settings.AreDevToolsEnabled = false;
-        //    settings.AreHostObjectsAllowed = false;
-        //    settings.HiddenPdfToolbarItems = Microsoft.Web.WebView2.Core.CoreWebView2PdfToolbarItems.None;
-        //    settings.IsBuiltInErrorPageEnabled = false;
-        //    settings.IsGeneralAutofillEnabled = false;
-        //    settings.IsPasswordAutosaveEnabled = false;
-        //    settings.IsPinchZoomEnabled = false;
-        //    settings.IsStatusBarEnabled = false;
-        //}
-        //#endif
-
     }
 
     protected override void OnDisappearing()
