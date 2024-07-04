04d9c28c6877bb4d252a6cfe2f3b54cb30e45fc7
Waleed <waleed.ali@rootpointers.com>
2024-07-04 17:22:38 +0500

    Fixed Api loading for large data

--- FirstTerraceSystems/Services/NasdaqService.cs
+++ FirstTerraceSystems/Services/NasdaqService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.Json;
 using FirstTerraceSystems.Entities;
 using FirstTerraceSystems.Features;
 using FirstTerraceSystems.Models;
@@ -6,68 +7,85 @@ using Newtonsoft.Json;
 
 namespace FirstTerraceSystems.Services
 {
-    public class NasdaqService
+  public class NasdaqService
+  {
+    private readonly HttpClient _client;
+
+    public NasdaqService(HttpClient client)
     {
-        private readonly HttpClient _client;
-
-        public NasdaqService(HttpClient client)
-        {
-            _client = client;
-        }
-
-        //NasdaqMarketFeed
-        public async Task<IEnumerable<MarketFeed>?> NasdaqGetDataAsync(DateTime startDatetime, string symbol)
-        {
-            string jsonData = System.Text.Json.JsonSerializer.Serialize(new { start_datetime = startDatetime.ToString(AppSettings.DFormat_NasdaqGetData), symbol = symbol });
-            HttpRequestMessage request = new(HttpMethod.Post, "/nasdaq/get_data")
-            {
-                Content = new StringContent(jsonData, Encoding.UTF8, "application/json")
-            };
-            try
-            {
-
-                HttpResponseMessage response = await _client.SendAsync(request);
-
-                response.EnsureSuccessStatusCode();
-
-                using (Stream responseStream = await response.Content.ReadAsStreamAsync())
-                {
-                    JsonSerializer? serializer = new Newtonsoft.Json.JsonSerializer();
-                    using StreamReader? streamReader = new StreamReader(responseStream);
-                    using JsonTextReader? textReader = new JsonTextReader(streamReader);
-                    return serializer.Deserialize<IEnumerable<MarketFeed>>(textReader);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return null;
-            }
-        }
-
-        public async Task<IEnumerable<NasdaqTicker>?> NasdaqGetTickersAsync()
-        {
-            HttpRequestMessage request = new(HttpMethod.Get, "/nasdaq/get_tickers");
-            try
-            {
-                HttpResponseMessage response = await _client.SendAsync(request);
-
-                response.EnsureSuccessStatusCode();
-
-                JsonSerializer? serializer = new Newtonsoft.Json.JsonSerializer();
-
-                using (Stream responseStream = await response.Content.ReadAsStreamAsync())
-                {
-                    using StreamReader? streamReader = new StreamReader(responseStream);
-                    using JsonTextReader? textReader = new JsonTextReader(streamReader);
-                    return serializer.Deserialize<IEnumerable<NasdaqTicker>>(textReader);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return null;
-            }
-        }
+      _client = client;
     }
+
+    //NasdaqMarketFeed
+    public async Task<IEnumerable<MarketFeed>?> NasdaqGetDataAsync(DateTime startDatetime, string symbol)
+    {
+      string jsonData = System.Text.Json.JsonSerializer.Serialize(new { start_datetime = startDatetime.ToString(AppSettings.DFormat_NasdaqGetData), symbol = symbol });
+      HttpRequestMessage request = new(HttpMethod.Post, "/nasdaq/get_data")
+      {
+        Content = new StringContent(jsonData, Encoding.UTF8, "application/json")
+      };
+      try
+      {
+
+        HttpResponseMessage response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+        response.EnsureSuccessStatusCode();
+
+        return await DeserializeStreamAsync(await response.Content.ReadAsStreamAsync());
+
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        return null;
+      }
+    }
+    private async Task<List<MarketFeed>> DeserializeStreamAsync(Stream stream)
+    {
+      var result = new List<MarketFeed>();
+      using var reader = new StreamReader(stream);
+      var json = await reader.ReadToEndAsync();
+      ProcessJson(json, result);
+      return result;
+    }
+
+    private void ProcessJson(string json, List<MarketFeed> result)
+    {
+      var jsonReader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json));
+
+      while (jsonReader.Read())
+      {
+        if (jsonReader.TokenType == JsonTokenType.StartObject)
+        {
+          var jsonElement = System.Text.Json.JsonSerializer.Deserialize<MarketFeed>(ref jsonReader);
+          result.Add(jsonElement);
+        }
+      }
+    }
+
+    public async Task<IEnumerable<NasdaqTicker>?> NasdaqGetTickersAsync()
+    {
+      HttpRequestMessage request = new(HttpMethod.Get, "/nasdaq/get_tickers");
+      try
+      {
+        HttpResponseMessage response = await _client.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        Newtonsoft.Json.JsonSerializer? serializer = new Newtonsoft.Json.JsonSerializer();
+
+        using (Stream responseStream = await response.Content.ReadAsStreamAsync())
+        {
+          using StreamReader? streamReader = new StreamReader(responseStream);
+          using JsonTextReader? textReader = new JsonTextReader(streamReader);
+          return serializer.Deserialize<IEnumerable<NasdaqTicker>>(textReader);
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        return null;
+      }
+    }
+  }
 }
